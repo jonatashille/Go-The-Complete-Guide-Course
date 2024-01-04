cf0e4b8f338c5ff76fcfffe7267b471c61b19d65
Jonatas Hille <jonatashille@gmail.com>
2024-01-04 17:05:32 -0300

    project done

---
+++ PracticeProject-PriceCalculator/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"Calculator/filemanager"
+	"Calculator/prices"
+	"fmt"
+)
+
+func main() {
+	var taxRates []float64 = []float64{0, 0.07, 0.1, 0.15}
+
+	for _, taxRate := range taxRates {
+		fm := filemanager.New("pricess.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		err := priceJob.Process()
+
+		if err != nil {
+			fmt.Println("Could not process job")
+			fmt.Println(err)
+		}
+	}
+
+}
