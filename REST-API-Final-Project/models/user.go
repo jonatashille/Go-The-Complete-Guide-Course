869358cdc356ae89338e1ada72b2028c17192cfb
Jonatas Hille <jonatashille@gmail.com>
2024-01-08 16:05:19 -0300

    login route

---
+++ REST-API-Final-Project/models/user.go
@@ -0,0 +1,86 @@
+package models
+
+import (
+	"REST-API/db"
+	"REST-API/utils"
+	"errors"
+)
+
+type User struct {
+	ID       int64
+	Email    string `binding:"required"`
+	Password string `binding:"required"`
+}
+
+func (u User) Save() error {
+	query := `
+	INSERT INTO users(email, password) VALUES (?, ?)
+	`
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
+	if err != nil {
+		return err
+	}
+
+	userId, err := result.LastInsertId()
+
+	u.ID = userId
+
+	return err
+}
+
+func GetAllUsers() ([]User, error) {
+	query := `SELECT * FROM users`
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []User
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Email, &user.Password)
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func (u User) ValidateCredentials() error {
+	query := `
+	SELECT password 
+	FROM users WHERE email = ?
+	`
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrivedPassword string
+	err := row.Scan(&retrivedPassword)
+	if err != nil {
+		return errors.New("credentials invalid")
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrivedPassword)
+	if !passwordIsValid {
+		return errors.New("credentials invalid")
+	}
+
+	return err
+}
---
+++ REST-API-Final-Project/routes/users.go
@@ -0,0 +1,54 @@
+package routes
+
+import (
+	"REST-API/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"Message": "Could not parse request data."})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"Message": "Could not save user."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User created successfuly"})
+}
+
+func getUsers(context *gin.Context) {
+	users, err := models.GetAllUsers()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch users. Try again later"})
+		return
+	}
+	context.JSON(http.StatusOK, users)
+}
+
+func login(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"Message": "Could not parse request data."})
+		return
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Could not authenticate user."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
+}
